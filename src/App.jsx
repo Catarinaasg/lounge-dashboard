559818ba2170456012f5a671fa37082bf256a345
Catarinaasg <62206141+Catarinaasg@users.noreply.github.com>
2025-10-20 11:04:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ export default function App() {
       <h2
         className="font-bold text-3xl"
         style={{
-          marginTop: "82px",
+          marginTop: "75px",
           marginBottom: "32px",
           fontFamily: "Geist, sans-serif",
         }}
